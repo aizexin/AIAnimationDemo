7fbf2c0e76dc3a763a2be1b9e774ac4937aec0e8
艾泽鑫 <443792431@qq.com>
2016-10-11 15:20:23 +0800

    第一层封装AFN3.0

---
+++ AIAnimationDemo/AIAnimationDemo/Class/NetWorking/AINetWorkHelper.h
@@ -0,0 +1,18 @@
+//
+//  AINetWorkHelper.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2016/10/11.
+//  Copyright © 2016年 艾泽鑫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef void (^SuccessBlock)(id response);
+typedef void (^FailureBlock)(NSError * err);
+
+
+@interface AINetWorkHelper : NSObject
++(void)GET:(NSString *)url parameters:(id)param success:(SuccessBlock)success failure:(FailureBlock)failure;
+
++(void)POST:(NSString *)url parameters:(id)param success:(SuccessBlock)success failure:(FailureBlock)failure;
+@end
