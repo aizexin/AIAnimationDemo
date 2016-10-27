296587127021cc9e6abceb6e55f8b5866b2b3f52
艾泽鑫 <443792431@qq.com>
2016-10-27 16:06:48 +0800

    实现发送和返回的判断

--- AIAnimationDemo/AIAnimationDemo/Class/Pop11/View/AIPictureCollectionViewCell.m
+++ AIAnimationDemo/AIAnimationDemo/Class/Pop11/View/AIPictureCollectionViewCell.m
@@ -12,6 +12,11 @@
 @interface AIPictureCollectionViewCell ()
 /** 是否在运动*/
 @property(assign,nonatomic,getter=isOnWindow)BOOL onWindow;
+
+/**
+ 一半距离
+ */
+@property(assign,nonatomic)CGFloat halfHight;
 @end
 
 @implementation AIPictureCollectionViewCell
@@ -57,12 +62,26 @@
     [recognizer setTranslation:CGPointMake(0, 0) inView:self.contentView];
     self.onWindow = YES;
     if (recognizer.state == UIGestureRecognizerStateEnded) {//松手的时候执行
+        
         //获得加速度
         CGPoint velocity = [recognizer velocityInView:self];
         //添加pop动画
         POPDecayAnimation *decayAnimation = [POPDecayAnimation animationWithPropertyNamed:kPOPLayerPositionY];
         decayAnimation.velocity = [NSValue valueWithCGPoint:velocity];
         [recognizer.view.layer pop_addAnimation:decayAnimation forKey:nil];
+        //返回最后的本地viewCenter的坐标
+        CGPoint endPoint =  [self.contentView convertPoint:recognizer.view.center fromView:lastWindow];
+        //判断距离
+        if (endPoint.y < 0) {//发送出去
+            AILog(@"发出去");
+        }else{//返回cell上
+            //一开始要记下frame，动画在window上做，完成后再加到contentView上
+            AILog(@"返回");
+            [self.contentView addSubview:recognizer.view];
+            recognizer.view.frame =  self.bounds;
+            self.onWindow = NO;
+        }
+        //返回或者，发送出去
     }
     
     
