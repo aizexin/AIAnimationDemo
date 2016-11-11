e8b5bd4ac7ced1f3508987b736860f85300b73e8
艾泽鑫 <443792431@qq.com>
2016-11-11 16:38:03 +0800

    整理代码，删除无用图片

--- AIAnimationDemo/AIAnimationDemo/Class/Pop16/Controller/AISettingViewController.m
+++ AIAnimationDemo/AIAnimationDemo/Class/Pop16/Controller/AISettingViewController.m
@@ -27,27 +27,32 @@
 -(NSMutableArray *)dataSource{
     if (!_dataSource) {
         _dataSource            = [NSMutableArray array];
+        
         //第一组
         NSMutableArray *group1 = [NSMutableArray array];
-        UIImageView *imageiew  = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"banben"]];
+        UIImageView *imageiew  = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"arrowRightLandscape"]];
+        imageiew.frame         = CGRectMake(0, 0, 10, 20);
         AISettingModel *model1 = [[AISettingModel alloc]initWithIcon:@"banben" title:@"版本" destClass:[AIDestOneViewController class] andAccessibilityView:imageiew];
         
         AISettingCellAdapter *adapter1 = [[AISettingModelAdapter alloc]initWithData:model1];
         [group1 addObject:adapter1];
+        
         //第二组
-        NSMutableArray *group2 = [NSMutableArray array];
-        UISwitch *switchBtn    = [[UISwitch alloc]init];
+        NSMutableArray *group2         = [NSMutableArray array];
+        UISwitch *switchBtn            = [[UISwitch alloc]init];
         [switchBtn addTarget:self action:@selector(onClickSwitch) forControlEvents:(UIControlEventTouchUpInside)];
-        AISettingModel *model2 = [[AISettingModel alloc]initWithIcon:@"changjing" title:@"场景" destClass:nil andAccessibilityView:switchBtn];
+        AISettingModel *model2         = [[AISettingModel alloc]initWithIcon:@"changjing" title:@"场景" destClass:nil andAccessibilityView:switchBtn];
         AISettingCellAdapter *adapter2 = [[AISettingModelAdapter alloc]initWithData:model2];
         [group2 addObject:adapter2];
         
-        AISettingModel *model3 = [[AISettingModel alloc]initWithIcon:@"guanyu" title:@"关于" destClass:nil andAccessibilityView:nil];
+        AISettingModel *model3          =\
+        [[AISettingModel alloc]initWithIcon:@"guanyu" title:@"关于" destClass:nil andAccessibilityView:nil];
         [model3 setBlock:^{
             AILog(@"点击关于");
         }];
-        AISettingCellAdapter *adapter3 = [[AISettingModelAdapter alloc]initWithData:model3];
+        AISettingCellAdapter *adapter3  = [[AISettingModelAdapter alloc]initWithData:model3];
         [group2 addObject:adapter3];
+        
         //第三组
         NSMutableArray *group3          = [NSMutableArray array];
         PPNumberButton *number          = [[PPNumberButton alloc]init];
