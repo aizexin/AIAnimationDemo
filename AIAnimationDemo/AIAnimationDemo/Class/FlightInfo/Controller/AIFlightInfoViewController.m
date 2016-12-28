589bc231dda3bbfbac536b43c0f438df6dfc7c4d
艾泽鑫 <443792431@qq.com>
2016-12-28 22:41:05 +0800

    添加背景切换动画

--- AIAnimationDemo/AIAnimationDemo/Class/FlightInfo/Controller/AIFlightInfoViewController.m
+++ AIAnimationDemo/AIAnimationDemo/Class/FlightInfo/Controller/AIFlightInfoViewController.m
@@ -13,7 +13,46 @@
 /**
  背景
  */
-@property(nonatomic,weak)UIImageView *bgImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *bgImageView;
+
+/**
+ 摘要
+ */
+@property (weak, nonatomic) IBOutlet UILabel *summary;
+
+/**
+ 摘要图片
+ */
+@property (weak, nonatomic) IBOutlet UIImageView *summaryIcon;
+@property (weak, nonatomic) IBOutlet UILabel *flightNrLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gateNrLabel;
+
+/**
+ 离开的地方
+ */
+@property (weak, nonatomic) IBOutlet UILabel *departingFromLabel;
+
+/**
+ 到达的地方
+ */
+@property (weak, nonatomic) IBOutlet UILabel *arrivingToLabel;
+
+/**
+ 飞机
+ */
+@property (weak, nonatomic) IBOutlet UIImageView *planeImage;
+
+/**
+ 飞行状态
+ */
+@property (weak, nonatomic) IBOutlet UILabel *flightStatusLabel;
+
+/**
+ 状态旗帜
+ */
+@property (weak, nonatomic) IBOutlet UIImageView *statusBannerImage;
+
+
 @end
 
 @implementation AIFlightInfoViewController
@@ -38,6 +77,27 @@
     [super viewWillDisappear:animated];
     self.navigationController.navigationBar.hidden = NO;
 }
+- (void)fadeImageView:(UIImageView *)imgeView toImage:(UIImage *)toImage showEffects:(BOOL)showEffects {
+    [UIView transitionWithView:imgeView duration:1. options:(UIViewAnimationOptionTransitionCrossDissolve) animations:^{
+        imgeView.image = toImage;
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    [UIView animateWithDuration:1. delay:0 options:(UIViewAnimationOptionCurveEaseOut) animations:^{
+//        self.snow
+    } completion:nil];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSString *imageString   = @"bg-sunny";
+    UIImage *toImage        = [UIImage imageNamed:imageString];
+    [self fadeImageView:self.bgImageView toImage:toImage showEffects:YES];
+    
+}
+
+
+
 
 
 
