6d453fa10b3c3cd7a2b194855879432dd961c0e2
艾泽鑫 <443792431@qq.com>
2017-01-16 23:01:44 +0800

    添加packlist文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/PackingList/Controller/AIPackingListViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIPackingListViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/1/16.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIPackingListViewController : AIBaseViewController
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/PackingList/Controller/AIPackingListViewController.m
@@ -0,0 +1,23 @@
+//
+//  AIPackingListViewController.m
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/1/16.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIPackingListViewController.h"
+
+@interface AIPackingListViewController ()
+
+@end
+
+@implementation AIPackingListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
