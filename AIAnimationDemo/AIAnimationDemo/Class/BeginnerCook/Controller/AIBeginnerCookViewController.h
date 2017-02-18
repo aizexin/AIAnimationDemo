dff37cfc9eb1becc851c681037bd22ea12ccd1d0
艾泽鑫 <443792431@qq.com>
2017-02-18 20:38:08 +0800

    添加BeginnerCook文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/BeginnerCook/Controller/AIBeginnerCookViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIBeginnerCookViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/2/18.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIBeginnerCookViewController : AIBaseViewController
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/BeginnerCook/Controller/AIBeginnerCookViewController.m
@@ -0,0 +1,24 @@
+//
+//  AIBeginnerCookViewController.m
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/2/18.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBeginnerCookViewController.h"
+
+@interface AIBeginnerCookViewController ()
+
+@end
+
+@implementation AIBeginnerCookViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+
+
+@end
