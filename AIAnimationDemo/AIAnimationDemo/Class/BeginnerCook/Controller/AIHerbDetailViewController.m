34de3ccfa80bf28a62b24bf3e74774a50cfa2351
艾泽鑫 <443792431@qq.com>
2017-03-13 23:35:43 +0800

    fix sth

--- AIAnimationDemo/AIAnimationDemo/Class/BeginnerCook/Controller/AIHerbDetailViewController.m
+++ AIAnimationDemo/AIAnimationDemo/Class/BeginnerCook/Controller/AIHerbDetailViewController.m
@@ -21,12 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
