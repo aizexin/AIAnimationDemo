228a6d2c7c4df562c51f7ea7e20dfe75b501ee79
艾泽鑫 <443792431@qq.com>
2017-03-23 23:10:42 +0800

    添加logoReveal

---
+++ AIAnimationDemo/AIAnimationDemo/Class/LogoReveal/Controller/AILogoRevealViewController.h
@@ -0,0 +1,13 @@
+//
+//  AILogoRevealViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/3/23.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AILogoRevealViewController : AIBaseViewController
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/LogoReveal/Controller/AILogoRevealViewController.m
@@ -0,0 +1,24 @@
+//
+//  AILogoRevealViewController.m
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/3/23.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AILogoRevealViewController.h"
+
+@interface AILogoRevealViewController ()
+
+@end
+
+@implementation AILogoRevealViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
