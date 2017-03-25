3a82582de66c68acc3aa364f4f3e5e13d2292c93
艾泽鑫 <443792431@qq.com>
2017-03-25 23:28:22 +0800

    添加文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/LogoReveal/Controller/AILogoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  AILogoDetailViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/3/25.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AILogoDetailViewController : UIViewController
+
+@end
