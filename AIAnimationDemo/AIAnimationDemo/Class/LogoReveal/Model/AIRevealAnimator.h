5f254136e6ce690410491c722ee494941ef268d6
艾泽鑫 <443792431@qq.com>
2017-03-26 23:10:38 +0800

    fix sth

---
+++ AIAnimationDemo/AIAnimationDemo/Class/LogoReveal/Model/AIRevealAnimator.h
@@ -0,0 +1,13 @@
+//
+//  AIRevealAnimator.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/3/26.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AIRevealAnimator : NSObject
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/LogoReveal/Model/AIRevealAnimator.m
@@ -0,0 +1,13 @@
+//
+//  AIRevealAnimator.m
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/3/26.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIRevealAnimator.h"
+
+@implementation AIRevealAnimator
+
+@end
