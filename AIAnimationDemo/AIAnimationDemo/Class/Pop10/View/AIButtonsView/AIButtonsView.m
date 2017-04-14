9dbef5188b7f452f74f5609ffea6ff8798f9f8d5
艾泽鑫 <443792431@qq.com>
2017-04-14 10:36:54 +0800

    修复警告

--- AIAnimationDemo/AIAnimationDemo/Class/Pop10/View/AIButtonsView/AIButtonsView.m
+++ AIAnimationDemo/AIAnimationDemo/Class/Pop10/View/AIButtonsView/AIButtonsView.m
@@ -13,8 +13,7 @@
 #define AIBtnMargin 8
 //最大个数
 #define AIBtnMaxCount 9
-// 随机色
-#define AIRandomColor [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
+
 static const CGFloat ButtonSize = 30.0;
 @interface AIButtonsView ()
 /** 一行的最多个数*/
