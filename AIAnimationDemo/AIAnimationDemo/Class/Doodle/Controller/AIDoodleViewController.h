7e615f81a649793328738e75fc2f412450044d21
艾泽鑫 <443792431@qq.com>
2017-04-17 21:42:11 +0800

    添加文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/Doodle/Controller/AIDoodleViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIDoodleViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/4/17.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIDoodleViewController : AIBaseViewController
+
+@end
