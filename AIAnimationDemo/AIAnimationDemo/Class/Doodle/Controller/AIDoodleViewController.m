8b3389d5b938c7dbc56b0657d2978f6b3d29daa5
艾泽鑫 <443792431@qq.com>
2017-04-26 23:08:34 +0800

    fix sth

--- AIAnimationDemo/AIAnimationDemo/Class/Doodle/Controller/AIDoodleViewController.m
+++ AIAnimationDemo/AIAnimationDemo/Class/Doodle/Controller/AIDoodleViewController.m
@@ -8,30 +8,49 @@
 
 #import "AIDoodleViewController.h"
 
-@interface AIDoodleViewController ()
+@interface AIDoodleViewController ()<UIGestureRecognizerDelegate>
 
+@property(nonatomic,strong)UIBezierPath *path;
+/** shaplayer*/
+@property(nonatomic,strong)CAShapeLayer *shapeLayer;
 @end
 
 @implementation AIDoodleViewController
 
+-(UIBezierPath *)path {
+    if (!_path) {
+        _path = [UIBezierPath bezierPath];
+    }
+    return _path;
+}
+-(CAShapeLayer *)shapeLayer {
+    if (!_shapeLayer) {
+        _shapeLayer = [CAShapeLayer layer];
+    }
+    return _shapeLayer;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UIPanGestureRecognizer *pan     = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(onPan:)];
+    pan.delegate                    = self;
+    
+    [self.view addGestureRecognizer:pan];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)onPan:(UIPanGestureRecognizer *)pan {
+   CGPoint point = [pan locationInView:pan.view];
+    [self.path addLineToPoint:point];
+    self.shapeLayer.path              = self.path.CGPath;
+    [self.view.layer addSublayer:self.shapeLayer];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark -UIGestureRecognizerDelegate
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    CGPoint point = [gestureRecognizer locationInView:self.view];
+    [self.path moveToPoint:point];
+    return YES;
 }
-*/
 
 @end
