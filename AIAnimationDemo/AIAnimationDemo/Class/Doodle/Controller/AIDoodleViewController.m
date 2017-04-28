e0245f6c584aaa11a54cbaf31d2ba1eefa13aee6
艾泽鑫 <443792431@qq.com>
2017-04-28 11:20:17 +0800

    替换画板

--- AIAnimationDemo/AIAnimationDemo/Class/Doodle/Controller/AIDoodleViewController.m
+++ AIAnimationDemo/AIAnimationDemo/Class/Doodle/Controller/AIDoodleViewController.m
@@ -7,50 +7,22 @@
 //
 
 #import "AIDoodleViewController.h"
-
+#import "AFBrushBoard.h"
+#import <MLTransition.h>
 @interface AIDoodleViewController ()<UIGestureRecognizerDelegate>
 
-@property(nonatomic,strong)UIBezierPath *path;
-/** shaplayer*/
-@property(nonatomic,strong)CAShapeLayer *shapeLayer;
 @end
 
 @implementation AIDoodleViewController
 
--(UIBezierPath *)path {
-    if (!_path) {
-        _path = [UIBezierPath bezierPath];
-    }
-    return _path;
-}
--(CAShapeLayer *)shapeLayer {
-    if (!_shapeLayer) {
-        _shapeLayer = [CAShapeLayer layer];
-    }
-    return _shapeLayer;
-}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIPanGestureRecognizer *pan     = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(onPan:)];
-    pan.delegate                    = self;
-    
-    [self.view addGestureRecognizer:pan];
+
+    AFBrushBoard *brushBoard = [[AFBrushBoard alloc]initWithFrame:self.view.frame];
+    [self.view addSubview:brushBoard];
+    brushBoard.disableMLTransition      = YES;
 }
 
 
--(void)onPan:(UIPanGestureRecognizer *)pan {
-   CGPoint point = [pan locationInView:pan.view];
-    [self.path addLineToPoint:point];
-    self.shapeLayer.path              = self.path.CGPath;
-    [self.view.layer addSublayer:self.shapeLayer];
-}
-
-#pragma mark -UIGestureRecognizerDelegate
-
-- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
-    CGPoint point = [gestureRecognizer locationInView:self.view];
-    [self.path moveToPoint:point];
-    return YES;
-}
 
 @end
