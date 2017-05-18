1776ad3b7bdffddd88d9b13609d192a4ceba7a0e
ken <sober_zhichao@163.com>
2017-05-19 07:35:10 +0800

    add file

---
+++ AIAnimationDemo/AIAnimationDemo/Class/OfficeBuddy/Controller/AIOfficeBuddyViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIOfficeBuddyViewController.h
+//  AIAnimationDemo
+//
+//  Created by Ken on 2017/5/19.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIOfficeBuddyViewController : AIBaseViewController
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/OfficeBuddy/Controller/AIOfficeBuddyViewController.m
@@ -0,0 +1,24 @@
+//
+//  AIOfficeBuddyViewController.m
+//  AIAnimationDemo
+//
+//  Created by Ken on 2017/5/19.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIOfficeBuddyViewController.h"
+
+@interface AIOfficeBuddyViewController ()
+
+@end
+
+@implementation AIOfficeBuddyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
