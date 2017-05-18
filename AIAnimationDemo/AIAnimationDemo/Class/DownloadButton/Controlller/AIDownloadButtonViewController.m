c57c8d178ffabc962dbe3f834ffe2b7fb6cae0b0
ken <sober_zhichao@163.com>
2017-05-18 23:22:55 +0800

    添加单位转换

--- AIAnimationDemo/AIAnimationDemo/Class/DownloadButton/Controlller/AIDownloadButtonViewController.m
+++ AIAnimationDemo/AIAnimationDemo/Class/DownloadButton/Controlller/AIDownloadButtonViewController.m
@@ -25,7 +25,7 @@
         AIWeakSelf
        self.task = [AFNetHelper downloadWithURL:@"http://wvideo.spriteapp.cn/video/2016/0116/569a048739c11_wpc.mp4" fileDir:@"xx" progress:^(NSProgress *progress) {
             weakSelf.downLoadButton.progress    = progress.fractionCompleted;
-            weakSelf.downLoadButton.text        = [NSString stringWithFormat:@"%lldkb",progress.completedUnitCount /1024];
+            weakSelf.downLoadButton.text        = [self transitionUnit:progress.completedUnitCount];;
         } success:^(NSString *filePath) {
             weakSelf.downLoadButton.progress    = 1.;
         } failure:^(NSError *err) {
@@ -39,6 +39,25 @@
     [self.downLoadButton resume];
 }
 
+/**
+ 转换单位
+
+ @param b 进来的时候是b
+ @return 返回单位 最大T
+ */
+- (NSString *)transitionUnit:(NSInteger)b {
+    NSArray *unit       = @[@"b",@"kb",@"mb",@"GB",@"T"];
+    CGFloat remainder   = 0.;
+    NSInteger i         = 0;
+    do {
+        remainder       = b / 1024.;
+        b               = remainder;
+        if (i < 5) {
+            i ++;
+        }
+    } while (remainder >= 1024);
+    return [NSString stringWithFormat:@"%.1lf%@",remainder,unit[i]];
+}
 
 
 
