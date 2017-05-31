bcdece2da105149ec4fef61a7881d47c0ce21f6e
艾泽鑫 <443792431@qq.com>
2017-05-31 20:16:01 +0800

    fix sth

---
+++ AIAnimationDemo/AIAnimationDemo/Class/OfficeBuddy/View/AIMenuItem.h
@@ -0,0 +1,13 @@
+//
+//  AIMenuItem.h
+//  AIAnimationDemo
+//
+//  Created by sober on 2017/5/31.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIMenuItem : UIView
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/OfficeBuddy/View/AIMenuItem.m
@@ -0,0 +1,21 @@
+//
+//  AIMenuItem.m
+//  AIAnimationDemo
+//
+//  Created by sober on 2017/5/31.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIMenuItem.h"
+
+@implementation AIMenuItem
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
