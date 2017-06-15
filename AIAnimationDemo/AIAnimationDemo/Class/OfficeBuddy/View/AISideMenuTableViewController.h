36914dcfa276167cd371cd928fe7bb07ecfebffc
aizexin <443792431@qq.com>
2017-06-15 23:41:57 +0800

    fix sth

---
+++ AIAnimationDemo/AIAnimationDemo/Class/OfficeBuddy/View/AISideMenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AISideMenuTableViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/6/15.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AISideMenuTableViewController : UITableViewController
+
+@end
