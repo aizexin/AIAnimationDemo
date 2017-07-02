ce88c5e4c74b48afcb056ed5f36e82da94352a50
aizexin <443792431@qq.com>
2017-07-02 22:24:55 +0800

    添加文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/Fold/Controller/AIFoldViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIFoldViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/7/2.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIFoldViewController : AIBaseViewController
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/Fold/Controller/AIFoldViewController.m
@@ -0,0 +1,24 @@
+//
+//  AIFoldViewController.m
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/7/2.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIFoldViewController.h"
+
+@interface AIFoldViewController ()
+
+@end
+
+@implementation AIFoldViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
