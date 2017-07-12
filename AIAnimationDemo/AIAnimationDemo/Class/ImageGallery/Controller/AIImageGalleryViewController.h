8a4e626952bfe81993503245bf9fa3a038ab8ad3
aizexin <443792431@qq.com>
2017-07-12 23:03:57 +0800

    添加文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/ImageGallery/Controller/AIImageGalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIImageGalleryViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/7/12.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIImageGalleryViewController : AIBaseViewController
+
+@end
