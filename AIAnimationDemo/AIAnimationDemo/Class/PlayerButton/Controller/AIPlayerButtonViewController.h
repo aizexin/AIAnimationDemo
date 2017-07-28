c329e92bf1783494029e2127bd0e695aff9355f3
aizexin <443792431@qq.com>
2017-07-28 22:49:28 +0800

    添加文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/PlayerButton/Controller/AIPlayerButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIPlayerButtonViewController.h
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/7/28.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIBaseViewController.h"
+
+@interface AIPlayerButtonViewController : AIBaseViewController
+
+@end
---
+++ AIAnimationDemo/AIAnimationDemo/Class/PlayerButton/Controller/AIPlayerButtonViewController.m
@@ -0,0 +1,24 @@
+//
+//  AIPlayerButtonViewController.m
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/7/28.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+#import "AIPlayerButtonViewController.h"
+
+@interface AIPlayerButtonViewController ()
+
+@end
+
+@implementation AIPlayerButtonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
