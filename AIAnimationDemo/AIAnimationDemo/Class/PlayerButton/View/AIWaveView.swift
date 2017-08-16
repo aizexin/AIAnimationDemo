8cb9a1ea458ae9a220aa67778899804ecb714aee
aizexin <443792431@qq.com>
2017-08-16 22:14:38 +0800

    添加文件

---
+++ AIAnimationDemo/AIAnimationDemo/Class/PlayerButton/View/AIWaveView.swift
@@ -0,0 +1,21 @@
+//
+//  AIWaveView.swift
+//  AIAnimationDemo
+//
+//  Created by 艾泽鑫 on 2017/8/16.
+//  Copyright © 2017年 艾泽鑫. All rights reserved.
+//
+
+import UIKit
+
+class AIWaveView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
