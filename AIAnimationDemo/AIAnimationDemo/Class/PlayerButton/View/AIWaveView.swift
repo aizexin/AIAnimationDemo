cd91da0785a3da756a28f25e09e9c9dda3a26ba0
艾泽鑫 <443792431@qq.com>
2017-08-25 22:10:44 +0800

    fix sth

--- AIAnimationDemo/AIAnimationDemo/Class/PlayerButton/View/AIWaveView.swift
+++ AIAnimationDemo/AIAnimationDemo/Class/PlayerButton/View/AIWaveView.swift
@@ -10,13 +10,6 @@ import UIKit
 
 class AIWaveView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.red
@@ -26,5 +19,4 @@ class AIWaveView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-
 }
